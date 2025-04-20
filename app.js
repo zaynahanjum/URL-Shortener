c7eff2f9f12d7e26f5829e6f8357bbb90ee602a0
zaynahanjum <84634851+zaynahanjum@users.noreply.github.com>
2025-04-20 22:24:04 +0530

    Update app.js (added port)

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const server = createServer (async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
 })
